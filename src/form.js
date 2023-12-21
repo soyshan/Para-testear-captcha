11249f2623491e8e04b7b2c70635d99a4c22a610
Shan <109847605+soyshan@users.noreply.github.com>
2023-12-21 09:07:19 +0100

    update form.js

--- src/form.js
+++ src/form.js
@@ -159,7 +159,7 @@ form.addEventListener('submit',(e)=>{
   const fd =new FormData(e.target);
   const params =new URLSearchParams(fd);
 
-  fetch ('https://clinquant-rabanadas-53a186.netlify.app/upload',{
+  fetch ('http://localhost:3000/upload',{
     method:"POST",
     body:params,
 
@@ -178,3 +178,57 @@ form.addEventListener('submit',(e)=>{
   .catch(err => console.error(err))
 
 })
+
+
+
+//Código necesario para que aparezca las imagenes en el captcha
+
+
+const express = require ('express');
+const cors = require('cors');
+const app= express();
+const port =3000;
+
+
+app.use(cors());
+
+app.use(express.urlencoded(
+    {
+        extend:false 
+    }
+))
+
+
+app.post('/upload', function(req, res){
+
+
+    const params = new URLSearchParams({
+        secret:'6LcB9TQpAAAAAN9czsF1XP84nPO_WvcLkz-pYb0R',
+    
+        response:req.body['g-recaptcha-response'],
+        remoteip: req.ip,
+    });
+    
+
+
+    fetch ('https://www.google.com/recaptcha/api/siteverify',{
+        method:"POST",
+        body:params,
+    })
+    .then(res =>res.json())
+    .then(data =>{
+        if (data.success){
+            res.json({captchaSuccess:true});
+        }
+        else{
+            res.json({captchaSuccess:false});
+        }
+    })
+
+
+});
+
+app.listen(port,()=>{
+    console.log(`App running on port ${port}`);
+})
+
