03a945b4ebe6282b5ce4bf986a14124d44bc2e07
Shan <109847605+soyshan@users.noreply.github.com>
2023-12-21 10:41:49 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,51 +1,46 @@
-const express = require ('express');
+const express = require('express');
 const cors = require('cors');
-const app= express();
-const port =3000;
+const fetch = require('node-fetch'); // Asegúrate de tener node-fetch instalado
 
+const app = express();
+const port = 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://soyshan.github.io',
+  methods: 'POST',
+  credentials: true,
+}));
 
-app.use(express.urlencoded(
-    {
-        extend:false 
-    }
-))
+app.use(express.urlencoded({ extended: false }));
 
+app.post('/upload', function (req, res) {
+  const params = new URLSearchParams({
+    secret: '6LekUzgpAAAAAGpPKUJGokol1hp_NZRFnAIu8N7R',
+    response: req.body['g-recaptcha-response'],
+    remoteip: req.ip,
+  });
 
-
-app.post('/upload', function(req, res){
-
-
-    const params = new URLSearchParams({
-        secret:'6LcB9TQpAAAAAN9czsF1XP84nPO_WvcLkz-pYb0R',
-    
-        response:req.body['g-recaptcha-response'],
-        remoteip: req.ip,
+  fetch('https://www.google.com/recaptcha/api/siteverify', {
+    method: 'POST',
+    body: params,
+  })
+    .then(res => res.json())
+    .then(data => {
+      if (data.success) {
+        res.json({ captchaSuccess: true });
+      } else {
+        res.json({ captchaSuccess: false });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ captchaSuccess: false });
     });
-    
-
-
-    fetch ('https://www.google.com/recaptcha/api/siteverify',{
-        method:"POST",
-        body:params,
-    })
-    .then(res =>res.json())
-    .then(data =>{
-        if (data.success){
-            res.json({captchaSuccess:true});
-        }
-        else{
-            res.json({captchaSuccess:false});
-        }
-    })
-
-
 });
 
-app.listen(port,()=>{
-    console.log(`App running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
 
 
 
